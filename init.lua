6749bc83331ee830dc10cf0f511b96023236f50a
spawnie <spawnie>
2025-10-19 12:27:15 +0200

    Add suda.vim

--- init.lua
+++ init.lua
@@ -179,6 +179,8 @@ require('lazy').setup({
     },
   },
 
+  { 'lambdalisue/suda.vim' },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
